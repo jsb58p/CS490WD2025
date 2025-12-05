dde60e20b8614c1f29657b859874e6f945127d3d
jsb58p <jacobbiddinger@gmail.com>
2025-12-05 15:17:13 -0600

    Initial commit for final deliverable 2

--- final/script.js
+++ final/script.js
@@ -1,5 +1,33 @@
 /* Jacob Biddinger -- 11/14/2025 */
+
+// Scroll to top of page button
+const button = document.getElementById("button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+	if (document.documentElement.scrollTop > 200) {
+		button.style.display = "block";
+	} else {
+		button.style.display = "none";
+	}
+}
+
+function handleClick(){
+	document.documentElement.scrollTop = 0;
+}
+
+button.addEventListener('click', handleClick);
+
+// Automatically update to current year in footer
 document.querySelector('.footer .year').textContent = new Date().getFullYear();
+
+// Expand details for projects and classes
+document.querySelectorAll('.project h3, .bio h2').forEach(header => {
+  header.addEventListener('click', () => {
+    header.parentElement.querySelector('.details').classList.toggle('show');
+  });
+});
 
 
 
